df4d3f5c446e87aabcdbebd302b007406dbbf787
ViggsV <130056822+ViggsV@users.noreply.github.com>
2025-05-29 11:33:36 +0100

    updating register and add

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-const eventRoutes = require('./routes/eventRoutes');
-const authRoutes = require('./routes/authRoutes');
+const eventRoutes = require("./routes/eventRoutes");
+const authRoutes = require("./routes/authRoutes");
 
-app.use('/event', eventRoutes);
-app.use('/auth', authRoutes);
+app.use("/events", eventRoutes);
+app.use("/auth", authRoutes);
 
 // Start the server
 app.listen(PORT, () => {
